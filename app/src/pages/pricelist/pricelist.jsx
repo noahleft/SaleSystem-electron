c003a01589fd6f1728f63309ec9da6a7f564b0bc
ShihFengLin <noahleft@gmail.com>
2022-01-10 00:29:53 +0800

    reorg directory

--- app/src/pages/pricelist/pricelist.jsx
+++ app/src/pages/pricelist/pricelist.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Container, Row, Stack } from "react-bootstrap";
-import CompSelect from "Components/dropdowns/compselect/compselect";
-import ProdSelect from "Components/dropdowns/prodselect/prodselect";
-import ClearSelect from "Components/dropdowns/clearselect/clearselect";
+import CompSelect from "Components/dropdowns/compselect";
+import ProdSelect from "Components/dropdowns/prodselect";
+import ClearSelect from "Components/dropdowns/clearselect";
 import PriceTable from "Components/tables/pricetable/pricetable";
 
 class PriceList extends React.Component {
