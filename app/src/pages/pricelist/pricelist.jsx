1384f6eb05128fb14fe5326a58e5e196550f579f
ShihFengLin <noahleft@gmail.com>
2022-01-09 22:37:59 +0800

    collect dropdowns to same directory

--- app/src/pages/pricelist/pricelist.jsx
+++ app/src/pages/pricelist/pricelist.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import CompSelect from "Components/compselect/compselect";
-import ProdSelect from "Components/prodselect/prodselect";
-import ClearSelect from "Components/clearselect/clearselect";
+import CompSelect from "Components/dropdowns/compselect/compselect";
+import ProdSelect from "Components/dropdowns/prodselect/prodselect";
+import ClearSelect from "Components/dropdowns/clearselect/clearselect";
 import PriceTable from "Components/pricetable/pricetable";
 
 class PriceList extends React.Component {
