01ab54372b6a2f30793322e62ac49f88b7b0c8cd
ShihFengLin <noahleft@gmail.com>
2022-02-06 18:32:31 +0800

    popup the prod list with price defined

--- app/src/components/recordform/formProd.jsx
+++ app/src/components/recordform/formProd.jsx
@@ -24,12 +24,49 @@ class FormProd extends React.Component {
     }
   }
 
+  getProductOptions(compId) {
+    if(compId==0) {
+      return this.props.productManager.productList.map(function(obj){
+        return (<option key={obj.ID} value={obj.ID}>{obj.NAME}</option>)
+      });
+    }
+    else {
+      function matchedPrice(compId) {
+        return function(obj) {
+          if(obj.COMP_ID == compId) return true;
+          return false;
+        }
+      }
+      const priceList = this.props.priceManager.priceList.filter(matchedPrice(compId));
+      const prodSet = new Set(priceList.map(p=>p.PROD_ID));
+      function inProdSet(prodSet) {
+        return function(obj) {
+          return prodSet.has(obj.ID);
+        }
+      }
+      function notInProdSet(prodSet) {
+        return function(obj) {
+          return ! prodSet.has(obj.ID);
+        }
+      }
+      let inSetProductList = this.props.productManager.productList.filter(inProdSet(prodSet));
+      let restProductList = this.props.productManager.productList.filter(notInProdSet(prodSet));
+      let content = inSetProductList.map(function(obj){
+        return (<option key={obj.ID} value={obj.ID}>{obj.NAME}</option>)
+      });
+      content.push(<option key={-1} disabled>-----------</option>);
+      content.push(restProductList.map(function(obj){
+        return (<option key={obj.ID} value={obj.ID}>{obj.NAME}</option>)
+      }));
+      return content;
+    }
+  }
+
   render() {
-    const content = this.props.productManager.productList.map(function(obj){
-      return (<option key={obj.ID} value={obj.ID}>{obj.NAME}</option>)
-    });
     const idx = this.props.recordManager.candidateRecordListIdx;
+    const comp_id = (idx!=-1)?this.props.recordManager.recordList[idx].COMP_ID:0;
     const prod_id = (idx!=-1)?this.props.recordManager.recordList[idx].PROD_ID:0;
+    const content = this.getProductOptions(comp_id);
     return (
     <Form.Group as={Row} className="mb-3" controlId="formProdName" ref="formProdName">
     <Form.Label column sm={4}>Product:</Form.Label>
