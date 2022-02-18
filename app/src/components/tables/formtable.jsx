5d23669b6c9be9f38b94040bf0423dfcf06bb9f9
ShihFengLin <noahleft@gmail.com>
2022-02-18 23:28:28 +0800

    use react-bootstrap-table to support sorting

--- app/src/components/tables/formtable.jsx
+++ app/src/components/tables/formtable.jsx
@@ -3,9 +3,11 @@ import ROUTES from "Constants/routes";
 import { changeCandidateFormListIdx } from "Redux/components/formManager/formManagerSlice";
 import { updateRecordList } from "Redux/components/recordManager/recordManagerSlice";
 import { connect } from "react-redux";
-import { Table, Button } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import { withTranslation } from "react-i18next";
 import "./tablesize.css";
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+import BootstrapTable from "react-bootstrap-table-next";
 
 function HighlightText(props) {
   if(props.highlight)
@@ -58,31 +60,79 @@ class FormTable extends React.Component {
     );
   }
 
+  renderNaviButton(obj) {
+    const { t } = this.props;
+    return (
+    <Button variant="primary" size="sm"
+      onClick={() => {
+        this.props.changeCandidateFormListIdx(obj.ID);
+        this.updateRecord(obj.ID);
+        this.props.onNavigate(ROUTES.RECORDLIST);
+      }} disabled={obj.DISABLE || this.props.formManager.requireSaving}>
+      {t("OpenIt")}
+    </Button>
+    )
+  }
+
+  genData(idx, obj) {
+    var rObj = {
+      idx: idx,
+      id: obj.ID,
+      name: (<HighlightText name={obj.NAME} highlight={obj.DIRTY}></HighlightText>),
+      quantity: numberWithCommas(obj.QUANTITY)+this.props.home.quantity_unit,
+      total: numberWithCommas(obj.SUM),
+      navi: this.renderNaviButton(obj),
+    };
+    return rObj;
+  }
+
   render() {
     let QuantityUnit = this.props.home.quantity_unit==""?"":"("+this.props.home.quantity_unit+")";
     const { t } = this.props;
-    let content = [];
     let formList = this.props.formManager.formList;
+
+    const columns = [{
+      dataField: 'id',
+      text: '#',
+      sort: true,
+      headerClasses: 'thID',
+    },{
+      dataField: 'name',
+      text: t("Name"),
+      headerClasses: 'thText',
+    },{
+      dataField: 'quantity',
+      text: t("Quantity")+QuantityUnit,
+      headerClasses: 'thNum',
+    },{
+      dataField: 'total',
+      text: t("Total"),
+      headerClasses: 'thNum',
+    },{
+      dataField: 'navi',
+      text: "",
+    }];
+
+    let data = [];
     for(let i=0; i<=formList.length-1; i++) {
-      content.push(this.genRow(i, formList[i]));
+      data.push(this.genData(i, formList[i]));
     }
+
+    const rowEvents = {
+      onClick: (e, row, rowIndex) => {
+        this.props.changeCandidateFormListIdx(row.idx);
+      }
+    }
+
+    const defaultSorted = [{
+      dataField: 'id',
+      order: 'desc',
+    }];
 
     return (
     <div className="scrollTable">
-    <Table bordered hover size="sm">
-    <thead>
-      <tr>
-        <th scope="col" className="thID">#</th>
-        <th scope="col" className="thText">{t("Name")}</th>
-        <th scope="col" className="thNum">{t("Quantity")+QuantityUnit}</th>
-        <th scope="col" className="thNum">{t("Total")}</th>
-        <th scope="col"></th>
-      </tr>
-    </thead>
-    <tbody>
-    {content}
-    </tbody>
-    </Table></div>
+      <BootstrapTable keyField="id" data= {data} columns={columns} rowEvents={rowEvents} defaultSorted={defaultSorted} hover bordered classes="table-sm" bootstrap4={true}/>
+    </div>
     );
   }
 }
