0b144c069d7db21148d0de4d0626fe2c3f07ef82
ShihFengLin <noahleft@gmail.com>
2022-03-02 22:41:03 +0800

    refactor

---
+++ app/src/components/config/configQuantityUnit.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { connect } from "react-redux";
+import { Form } from "react-bootstrap";
+import { updateQuantityUnit } from "Redux/components/home/homeSlice";
+import { writeConfigRequest } from "secure-electron-store";
+
+class EditQuantityUnit extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        value: "",
+      }
+      this.handleSubmit = this.handleSubmit.bind(this);
+      this.handleChange = this.handleChange.bind(this);
+    }
+  
+    handleSubmit(e) {
+      window.api.store.send(writeConfigRequest, "quantity_unit", this.state.value);
+      this.props.updateQuantityUnit(this.state.value);
+      e.preventDefault();
+    }
+  
+    handleChange(e) {
+      this.setState({value: e.target.value});
+    }
+  
+    render() {
+      return (
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Group controlId="editValue">
+            <Form.Control type="text" value={this.state.value} onChange={this.handleChange}/>
+          </Form.Group>
+        </Form>
+      )
+    }
+}
+
+const mapStateToProps = (state, props) => ({
+    home: state.home
+});
+const mapDispatch = { updateQuantityUnit };
+
+export default connect(mapStateToProps, mapDispatch)(EditQuantityUnit);
--- app/src/components/tables/settingtable.jsx
+++ app/src/components/tables/settingtable.jsx
@@ -1,50 +1,11 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Form, Table } from "react-bootstrap";
-import { updateQuantityUnit } from "Redux/components/home/homeSlice";
+import { Table } from "react-bootstrap";
 import { withTranslation } from "react-i18next";
-import { writeConfigRequest } from "secure-electron-store";
+import EditQuantityUnit from "Components/config/configQuantityUnit";
 import "./tablesize.css";
 
-class EditQantityUnit extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: "",
-    }
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleSubmit(e) {
-    window.api.store.send(writeConfigRequest, "quantity_unit", this.state.value);
-    this.props.updateFunc(this.state.value);
-    e.preventDefault();
-  }
-
-  handleChange(e) {
-    this.setState({value: e.target.value});
-  }
-
-  render() {
-    return (
-      <Form onSubmit={this.handleSubmit}>
-        <Form.Group controlId="editValue">
-          <Form.Control type="text" value={this.state.value} onChange={this.handleChange}/>
-        </Form.Group>
-      </Form>
-    )
-  }
-}
-
 class SettingTable extends React.Component {
-  componentWillUnmount() {
-    // Clear any existing bindings;
-    // important on mac-os if the app is suspended
-    // and resumed. Existing subscriptions must be cleared
-    window.api.contextMenu.clearRendererBindings();
-  }
-
   render() {
     const { t } = this.props;
     return (
@@ -60,7 +21,7 @@ class SettingTable extends React.Component {
       <tr key={1}>
         <th scope="row">{t("QuantityUnit")}</th>
         <td>{this.props.home.quantity_unit}</td>
-        <td><EditQantityUnit updateFunc={this.props.updateQuantityUnit}/></td>
+        <td><EditQuantityUnit/></td>
       </tr>
     </tbody>
     </Table>
@@ -71,6 +32,6 @@ class SettingTable extends React.Component {
 const mapStateToProps = (state, props) => ({
   home: state.home
 });
-const mapDispatch = { updateQuantityUnit };
+const mapDispatch = { };
 
 export default connect(mapStateToProps, mapDispatch)(withTranslation()(SettingTable));
